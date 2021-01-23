904603fd60538a22d133a8a0996b3be84e7cdf0a
den.jabs <dennisjabs1@outlook.de>
2021-01-23 19:08:04 +0100

    Added dummy command handlers

--- bot.py
+++ bot.py
@@ -1,14 +1,53 @@
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    # prints something, if the bot is ready
-    async def on_ready(self):
-        print('Logged on as {0}'.format(self.user))
+bot = commands.Bot(command_prefix="!")
 
-    # prints the message on the console that the bot gets in Discord 
-    async def on_message(self, message):
-        print('Message from {0.author}({0.author.id}):{0.content}'.format(message))
+## all events are listed here ## 
 
-client = MyClient()
+# prints something, if the bot is ready
+@bot.event
+async def on_ready():
+    print('Logged on as {0}'.format(bot.user))
+
+# prints a hello message, if the user prints 'Hello' in the discord chat 
+@bot.event
+async def on_message(message):
+    if (message.author == bot.user): # to stop infinite loop it if the bot is sending a message
+        return
+    if (message.channel.id == 248897274002931722): # channel id of textchannel 'chat'
+        if(message.content.startswith('Hello')):
+            await message.channel.send('Hello {0.author}'.format(message))
+        await bot.process_commands(message) # has to be called for command handler
+
+## all comments are listed here ## 
+@bot.command()
+async def mute(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def kick(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def move(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def mute(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def ban(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def remove(ctx,arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def delete(ctx,arg):
+    await ctx.send(arg)
+
 # run the bot on the server
-client.run('NTkzMDU4MTI5NzIyMjEyMzY0.XRIWbA.yAmZ1vhg_j9rV1OklbxA9ZjL4J8')
+bot.run('NTkzMDU4MTI5NzIyMjEyMzY0.XRIWbA.yAmZ1vhg_j9rV1OklbxA9ZjL4J8')
