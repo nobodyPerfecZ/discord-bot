46c2a5a0fc038a5fbcd9d597e7ba5457b53a6de7
dennis <dennisjabs1@outlook.de>
2021-10-12 23:48:09 +0200

    Added musicbot

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+import music
+
+client = commands.Bot(command_prefix='!', intents= discord.Intents.all())
+
+cogs = [music]
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+# run the bot on the server
+client.run('NTkzMDU4MTI5NzIyMjEyMzY0.XRIWbA.yAmZ1vhg_j9rV1OklbxA9ZjL4J8')
