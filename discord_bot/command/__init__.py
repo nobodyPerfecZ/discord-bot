78beec6175ae11260107b39469ad278e329a11b2
Dennis Jabs <dennis-jabs@outlook.de>
2025-06-29 12:18:23 +0200

    D. J.: - Moved from black, flake8 and isort to ruff - Fixed linting errors

--- discord_bot/command/__init__.py
+++ discord_bot/command/__init__.py
@@ -1,7 +1,7 @@
+from discord_bot.command.chat import Chat
 from discord_bot.command.disconnect import Disconnect
 from discord_bot.command.manager import Manager
 from discord_bot.command.music import Music
-from discord_bot.command.chat import Chat
 
 __all__ = ["Chat", "Disconnect", "Manager", "Music"]
 
--- discord_bot/transformer/__init__.py
+++ discord_bot/transformer/__init__.py
@@ -1,6 +1,5 @@
 from discord_bot.transformer.ytdl_transformer import YTDLVolumeTransformer
 
-
 __all__ = ["YTDLVolumeTransformer"]
 
 assert __all__ == sorted(__all__), f"__all__ needs to be sorted into {sorted(__all__)}!"
